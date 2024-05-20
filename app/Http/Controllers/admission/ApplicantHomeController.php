a9559dd7b532e4f73e0567dc6d972c37107629e8
ajanta <ajanta.au@iitism.ac.in>
2024-05-20 20:55:15 +0530

    Changes made on 20-05-2024

--- app/Http/Controllers/admission/ApplicantHomeController.php
+++ app/Http/Controllers/admission/ApplicantHomeController.php
@@ -29,12 +29,74 @@ class ApplicantHomeController extends Controller {
     public $appl_ms="adm_phdef_appl_ms";
     public $email_log="adm_phdef_email_log";
     public $error_log="adm_phdef_error_log";
+    public $tab = "adm_phdef_tab";
 
     public function getAppHomeDetails(Request $request){
         try{
+            DB::enableQueryLog();
+            $validateUser = validator::make($request->all(),[
+                'registration_no' => 'required|alpha_num:ascii',
+                'email' => 'required|email'
+            ]);
+
+            if ($validateUser->fails()) {
+                return response()->json([
+                   'status' => false,
+                   'message' => 'validation error',
+                   'errors' => $validateUser->messages()
+                ],200);
+            }
+            else {
+                $email = $request['email'];
+                $registration_no = $request['registration_no'];
+                $data_fetch_prog_details = DB::select("select prog.* from adm_phdef_reg_appl_program prog where registration_no = '".$registration_no."'");
+                if (!empty($data_fetch_prog_details)) {
+                    $appl_program = $data_fetch_prog_details;
+                }
+                else {
+                    $appl_program = "";
+                }
+
+               $check_program_apply_details_count = DB::table($this->tab)->where('registration_no',$registration_no)->where('prom_apply_status','program applied')->count();
+               $get_program_details_apply = DB::table($this->tab)->where('registration_no',$registration_no)->where('prom_apply_status','program applied')->get();
+               /* how to process the last query */
+            //    // Retrieve the query log
+            //    $queryLog = DB::getQueryLog();
+            //    // Get the last query
+            //    $lastQuery = end($queryLog);
+            //    // Output the last query
+            //    dd($lastQuery);
+               /* how to process the last query */
+               if ($check_program_apply_details_count > 0) {
+                $check_program_apply = $get_program_details_apply;
+               }
+               else{
+                $check_program_apply = '';
+               }
+
+              $check_tab_position_details = DB::table($this->tab)->where('registration_no',$registration_no)->get();
+              if (!empty($check_tab_position_details)) {
+                 $tab_position = $check_tab_position_details;
+              }
+              else {
+                $tab_position = '';
+              }
+
+              $tab= DB::select("select GREATEST(IFNULL(tab1,0),IFNULL(tab2,0),IFNULL(tab3,0),IFNULL(tab4,0)) hightest from adm_phdef_tab where registration_no = '".$registration_no."'");
+            //      // Retrieve the query log
+            //    $queryLog = DB::getQueryLog();
+            //    // Get the last query
+            //    $lastQuery = end($queryLog);
+            //    // Output the last query
+            //    dd($lastQuery);
+              if (!empty($tab)) {
+                $value = $tab[0]->highest;
+
+              }
 
 
 
+            }
         }
         catch(QueryException $ex){
             return response()->json([
