f6d50af6ba733005f06940d71773047285be1ce6
ajanta <ajanta.au@iitism.ac.in>
2024-05-23 00:37:13 +0530

    23-05-2024

--- app/Http/Controllers/admission/ApplicantHomeController.php
+++ app/Http/Controllers/admission/ApplicantHomeController.php
@@ -83,18 +83,75 @@ class ApplicantHomeController extends Controller {
               }
 
               $tab= DB::select("select GREATEST(IFNULL(tab1,0),IFNULL(tab2,0),IFNULL(tab3,0),IFNULL(tab4,0)) hightest from adm_phdef_tab where registration_no = '".$registration_no."'");
+              $get_applicant_name = DB::table("adm_phdef_registration")->select('first_name','middle_name','last_name')->where('registration_no',$registration_no)->get();
+              $applicant_name = $get_applicant_name[0]->first_name;
+              if ($get_applicant_name[0]->middle_name != '') {
+                $applicant_name .= ' '.$get_applicant_name[0]->middle_name;
+              }
+              if ($get_applicant_name[0]->last_name != '') {
+                $applicant_name .= ' '.$get_applicant_name[0]->last_name;
+              }
+            //   echo $applicant_name; exit;
+              echo '<pre>';
+              print_r($tab[0]);
+              echo '</pre>';
+              exit;
             //      // Retrieve the query log
             //    $queryLog = DB::getQueryLog();
             //    // Get the last query
             //    $lastQuery = end($queryLog);
             //    // Output the last query
             //    dd($lastQuery);
+            // echo '<pre>';
+            // print_r($tab);
+            // echo '</pre>';
+            // exit;
+            //$applicant_name =
               if (!empty($tab)) {
                 $value = $tab[0]->highest;
+                $data['tab'] = $tab[0]->highest;
+                $data['name'] = $applicant_name;
+                $data['p_apply'] = $tab_position[0]->prom_apply_status;
+              }
+              else {
+                $data = array();
+              }
+
+              //exit;
+
+              #$data['val'] = "H";
+              $data['gate_paper_code'] = DB::table("adm_phdef_gate_paper")->get()->toArray();
+              $data['btech_paper'] = DB::table("adm_phdef_program_ms")->get()->toArray();
+              $get_candidate_type = DB::table("adm_phdef_registration")->select('appl_type')->where('registration_no',$registration_no)->first();
+              $data['candidate_type'] = $get_candidate_type->appl_type;
+              $data['appl_ms'] = DB::table("adm_phdef_appl_ms")->where('registration_no',$registration_no)->get()->toArray();
+              $data['program_ms'] = DB::table("adm_phdef_reg_appl_program")->where('registration_no',$registration_no)->get()->toArray();
+              $data['login_type'] = 'phdef';
+
+            //   if (empty($data['appl_ms'])) {
+            //       echo "appl ms data is empty"; //exit;
+            //   }
+
+            //   if (empty($data['program_ms'])) {
+            //      echo "program ms data is empty"; //exit;
+            //   }
+
+            //   exit;
+
+              if (empty($data['appl_ms'])) {
+                 $data['val'] = "H";
+                 $this->applications_track($applicant_name,$email,$registration_no,$data['login_type']); // For Testing purpose!!
+              }
+              else {
 
               }
 
-
+              #print_r($data); exit;
+              return response()->json([
+                'status' => true,
+                'message' => 'user_dashboard_details',
+                'data' => $data
+              ]);
 
             }
         }
@@ -105,6 +162,29 @@ class ApplicantHomeController extends Controller {
                 'errors' => $ex->getMessage()
             ]);
 
-        }
+          }
+      }
+
+      public function applications_track($applicant_name,$email,$registration_no,$login_type){
+
+         try {
+           if (empty($applicant_name) && empty($email) && empty($registration_no) && empty($login_type)) {
+              throw new Exception('Please provide valid candidate details');
+           }
+           else {
+
+             $gettabdetails = DB::table($this->tab)->select(DB::raw('count(*) as row_count'))->where('registration_no',$registration_no)->get()->toArray();
+             #$gettabdetailscount = DB::table($this->tab)->where('registration_no',$registration_no)->count();
+
+             if ($gettabdetailscount > 0) {
+                $data['tabdetails'] = $gettabdetails;
+             }
+           }
+         } catch (Exception $ex) {
+             echo $ex->getMessage();
+         }
+
+
+      }
+
     }
-}
